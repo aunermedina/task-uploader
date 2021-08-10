b0c56646063571f4debeb0f44365829c45c9cb34
aunermedina <auner.montejano@gmail.com>
2021-08-10 15:20:08 -0500

    new lables are based on sheet name

--- task_uploader.py
+++ task_uploader.py
@@ -1,7 +1,7 @@
 import os
 import argparse
 import logging
-from todoist.api import TodoistAPI
+from todoist.api import SyncError, TodoistAPI
 from openpyxl import load_workbook
 from datetime import datetime
 from dotenv import load_dotenv
@@ -9,7 +9,7 @@ from random import randint
 
 load_dotenv()
 
-log_cli_format = "%(asctime)s [%(levelname)8s] %(message)s (%(filename)s:%(lineno)s)"
+log_cli_format = "%(asctime)s [%(levelname)8s] %(message)s" # (%(filename)s:%(lineno)s)"
 log_cli_date_format = "%Y-%m-%d %H:%M:%S"
 
 logging.basicConfig(format=log_cli_format, datefmt=log_cli_date_format, level=logging.INFO)
@@ -38,23 +38,26 @@ def todoist_task_uploader(cal, fin):
     # read file
     logging.info('Reading file...')
     wb = load_workbook(fin)
-    logging.info('Creating new project, sections, tasks...')
+    logging.info('Creating new project...')
     # Create new Semester Project
     semester = api.projects.add('Semester ' + cal, parent_id=school_pid, color=randint(30, 49))
 
     for materia in wb.worksheets:
         # Create Section per Subject(Worksheet)
-        section = api.sections.add(materia.title, semester['id'])
+        subject = materia.title.split('-')
+        section = api.sections.add(subject[0], semester['id'])
+        new_label = api.labels.add(subject[1])
+        label_id = 2155146353 if subject[1] == 'proyecto' else new_label['id']
         subject_count += 1
-
+        logging.info('Creating section: ' + subject[0])
+        logging.info('Creating tasks...')
         for task in materia.values:
-            label = api.labels.add(task[2])
             # Create Tasks Per Section
             api.items.add(task[0],
                           due={"date": datetime.strftime(task[1], '%Y-%m-%d')},
-                          project_id=school_pid,
+        #                   project_id=school_pid,
                           section_id=section['id'],
-                          labels=[2155113156, 2155146353 if task[2] == 'proyecto' else label['id']])  # label 'INICIO' and section label
+                          labels=[2155113156, label_id])  # label 'INICIO' and section label
             task_count += 1
 
     # Commit Changes and Sync
@@ -67,18 +70,27 @@ def todoist_task_uploader(cal, fin):
     
 
 if __name__ == "__main__":
-    if os.getenv('DEBUG'):
-        logging.debug('Debugging')
-        api = TodoistAPI(os.getenv('API_TOKEN'))
-        logging.debug('Connection established!')
-        api.sync()
-        # playground down here
+    # if os.getenv('DEBUG') == 'true':
+    #     logging.debug('Debugging')
+    #     api = TodoistAPI(os.getenv('API_TOKEN'))
+    #     logging.debug('Connection established!')
+    #     api.sync()
+    #     # playground down here
+    #     # Search for desire Project (SCHOOL)
+    #     for project in api.state['projects']:
+    #         if 'SCHOOL' in project['name']:
+    #             school_pid = project['id']
         
-    else:    
-        # Define or gather the options and file to use
-        parser = argparse.ArgumentParser(prog='task_uploader', description="Create tasks in Todoist app based on xlsx file")
-        parser.add_argument("-c", "--calendar", help="Calendar Description. e.g. 2020B", required=True)
-        parser.add_argument("-f", "--file", help="A file to process", required=True)
-        args = parser.parse_args()
+    #     semester = api.projects.add('Semester test', parent_id=school_pid, color=randint(30, 49))
+    #     api.commit()
+    #     api.sync()
 
-        todoist_task_uploader(args.calendar, args.file)
+    # else:    
+        # Define or gather the options and file to use
+    logging.info('Starting configuration...')
+    parser = argparse.ArgumentParser(prog='task_uploader', description="Create tasks in Todoist app based on xlsx file")
+    parser.add_argument("-c", "--calendar", help="Calendar Description. e.g. 2020B", required=True)
+    parser.add_argument("-f", "--file", help="A file to process", required=True)
+    args = parser.parse_args()
+
+    todoist_task_uploader(args.calendar, args.file)
